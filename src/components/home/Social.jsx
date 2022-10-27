d0695abc2797b828e2720d2e57bbf6df9c780279
gkehdud <41028214+gkehdud@users.noreply.github.com>
2022-10-27 14:23:05 -0400

    font changed

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 const Social = () => {
     return (
-        <div data-aos = "fade-right">
-        <div className="home__social">
+        <div data-aos="fade-right">
+            <div className="home__social">
 
-                <a href="https://www.instagram.com/doyoun_g" className="home__social-icon" target="_blank">
+                {/* <a href="https://www.instagram.com/doyoun_g" className="home__social-icon" target="_blank">
                     <i className="uil uil-instagram"></i>
+                </a>
+
+                <a href="https://www.facebook.com/gkehdud" className="home__social-icon" target="_blank">
+                    <i className="uil uil-facebook"></i>
                 </a>
 
                 <a href="https://www.linkedin.com/in/doyoung-ha-b64352238/" className="home__social-icon" target="_blank">
@@ -14,9 +18,25 @@ const Social = () => {
 
                 <a href="https://www.github.com/gkehdud" className="home__social-icon" target="_blank">
                     <i className="uil uil-github-alt"></i>
+                </a> */}
+
+                <a href="https://www.instagram.com/doyoun_g" className="home__social-icon" target="_blank">
+                    <i className="bx bxl-instagram"></i>
                 </a>
 
-        </div>
+                <a href="https://www.facebook.com/gkehdud" className="home__social-icon" target="_blank">
+                    <i className="bx bxl-facebook"></i>
+                </a>
+
+                <a href="https://www.linkedin.com/in/doyoung-ha-b64352238/" className="home__social-icon" target="_blank">
+                    <i className="bx bxl-linkedin"></i>
+                </a>
+
+                <a href="https://www.github.com/gkehdud" className="home__social-icon" target="_blank">
+                    <i className="bx bxl-github"></i>
+                </a>
+
+            </div>
         </div>
     )
 }
